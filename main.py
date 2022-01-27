4dce71a485766dffebb6361b242d317f1f3f6d6d
hammed103 <hammedfree@gmail.com>
2022-01-27 12:27:33 +0100

    less

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import numpy as np
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException ,StaleElementReferenceException,InvalidArgumentException
 from selenium.common.exceptions import NoSuchElementException,ElementClickInterceptedException
-from webdriver_manager.chrome import ChromeDriverManager as CM
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -98,30 +97,29 @@ def selex(idx, x):
 
 #authorization
 #Obtained freely from googlesheetapi
-gc = pygsheets.authorize(service_file='my-project-1515950162194-4db978de441c.json')
+gc = pygsheets.authorize(service_file='new/my-project-1515950162194-4db978de441c.json')
 
 
 # In[ ]:
 
 
-interval = 10 #seconds
+interval = 2 #seconds
 iterations = 10 #times
 
 
 # In[31]:
+def runner():
+    while (iterations > 0):
+        sh = gc.open('Monitoro Tracker')
 
+        #select the first sheet
+        wks = sh[0]
+        rt = pd.DataFrame(wks.get_all_records())
+        ft = rt.copy()
+        ft = ft.reset_index().rename(columns = {'index':'indexz'})
 
-while (iterations > 0):
-    sh = gc.open('Monitoro Tracker')
-
-    #select the first sheet
-    wks = sh[0]
-    rt = pd.DataFrame(wks.get_all_records())
-    ft = rt.copy()
-    ft = ft.reset_index().rename(columns = {'index':'indexz'})
-
-    ft.apply(lambda x : selex(x.indexz,x['Order URL']), axis=1)
-    sleep(interval)
-
-
-# In[ ]:
+        ft.apply(lambda x : selex(x.indexz,x['Order URL']), axis=1)
+        sleep(interval)
+    # In[ ]:
+if __name__ == '__main__':
+    runner()
