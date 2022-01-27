256a726d7e016b049db3def129f3576e65a5f4a6
hammed103 <hammedfree@gmail.com>
2022-01-27 14:24:56 +0100

    worker

--- main.py
+++ main.py
@@ -25,17 +25,12 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import pygsheets
 import pandas as pd
 
-
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-chrome_options = Options()
 chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-
-
-chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 #chrome_options.headless = True # also works
 
