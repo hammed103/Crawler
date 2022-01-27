8e8a0f89c9ae2b9b5653c1d27145515f379697c1
hammed103 <hammedfree@gmail.com>
2022-01-27 14:05:34 +0100

    new

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = Options()
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 
